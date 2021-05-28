c60f858fbc13273126cbc357c9dcb6a8598ee608
Daniel <daniel.motowilczuk@gmail.com>
2021-05-28 17:13:17 +0200

    new

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return app.send_static_file('about.html')
+
+@app.route('/gallery')
+def gallery():
+    return app.send_static_file('gallery.html')
+
+@app.route('/contact')
+def contact():
+    return app.send_static_file('contact.html')
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'),404
+
+if __name__ == '__main__':
+    app.run(debug=True)
